467a6c8c12ed91b5d96b6f14b634ad3693d50348
Kashyap Paidimarri <kashyapp@gmail.com>
2013-03-12 13:10:17 +0530

    Many small changes
    1. Use nanos until the end
    2. Reuse executor service, resize when needed
    3. Cycle through input file in Step command
    4. Use ByteStreams.copy in Fist
    5. Embed $JAVA_OPTS placeholder into the executable jar
    6. Fix request rate calculations

--- src/main/java/com/ning/pummel/cli/Step.java
+++ src/main/java/com/ning/pummel/cli/Step.java
@@ -21,14 +21,24 @@ import java.io.StringReader;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ThreadPoolExecutor;
 
-@Command(name = "step")
+import static com.google.common.collect.Iterables.addAll;
+import static com.google.common.collect.Iterables.limit;
+import static com.google.common.collect.Iterables.cycle;
+import static com.ning.pummel.cli.Benchmark.nsToMs;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+@Command(name = "step", description = "Step through concurrency levels and report statistics")
 public class Step implements Callable<Void>
 {
+    @Option(name = {"-c", "--cycle"}, title = "cycle/loop", description = "Cycle or loop through url file?")
+    public boolean cycle = false;
+
     @Option(name = {"-m", "--max"}, title = "max-requests", description = "Maximum number of requests to execute")
     public int maxRequests = -1;
 
-    @Option(name = {"-s", "--start"}, description = "initial concurrency level, defaults to 100")
+    @Option(name = {"-s", "--start"}, description = "initial concurrency level, defaults to 1")
     public int start = 1;
 
     @Option(name = "--step", title = "step-function", description = "clojure function to apply to generate next step, default is 'c + 1'")
@@ -40,7 +50,7 @@ public class Step implements Callable<Void>
     @Option(name = {"-L", "--limit"}, description = "concurrency limit to stop at, default is " + Integer.MAX_VALUE)
     public int limit = Integer.MAX_VALUE;
 
-    @Option(name = {"-p", "--percentile"}, description = "Percentile to try to target, default is 99th percentile")
+    @Option(name = {"-p", "--percentile"}, description = "Percentile to report, default is 99th percentile")
     public double percentile = 99.0;
 
     @Arguments(title = "url file", description = "input file to pull urls from, otherwise will use stdin")
@@ -55,19 +65,16 @@ public class Step implements Callable<Void>
         }
         else {
             in = new BufferedReader(new InputStreamReader(System.in));
-        }
-        for (String line = in.readLine(); line != null; line = in.readLine()) {
-            if (maxRequests >= 0) {
-                if (maxRequests == 0) {
-                    break;
-                }
-                else if (maxRequests > 0) {
-                    maxRequests--;
-                }
-            }
+		}
+
+        for (String line = in.readLine(); line != null && maxRequests != 0; line = in.readLine(), maxRequests--) {
             urls.add(line);
         }
 
+        if (cycle && maxRequests > 0) {
+            List<String> urlsCopy = Lists.newArrayList(urls);
+            addAll(urls, limit(cycle(urlsCopy), maxRequests));
+        }
 
         Optional<StepFunction> of = mvel(stepFunction);
         if (!of.isPresent()) {
@@ -83,13 +90,18 @@ public class Step implements Callable<Void>
 
         if (labels) {System.out.printf("clients\ttp%.1f\tmean\treqs/sec\n", percentile);}
         int concurrency = start;
+        ThreadPoolExecutor exec = Fight.threadPoolExecutor();
+
         do {
-            DescriptiveStatistics stats = new Fight(concurrency, urls).call();
+            long start = System.nanoTime();
+            DescriptiveStatistics stats = new Fight(exec, concurrency, urls).call();
+            long stop = System.nanoTime();
+            long duration_nanos = stop - start;
             System.out.printf("%d\t%.2f\t%.2f\t%.2f\n",
                               concurrency,
-                              stats.getPercentile(percentile),
-                              stats.getMean(),
-                              (1000 / stats.getMean()) * concurrency);
+                              nsToMs(stats.getPercentile(percentile)),
+                              nsToMs(stats.getMean()),
+                              ((double)stats.getN()) * SECONDS.toNanos(1) / duration_nanos);
             concurrency = step.step(concurrency);
         }
         while (concurrency < limit);
